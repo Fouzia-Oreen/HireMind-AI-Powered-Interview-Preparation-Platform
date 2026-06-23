e7db278e7d9ece4d041666b99daaeecec218ff03
Fouzia Oreen <fouziaoreen@gmail.com>
2026-06-23 22:30:16 +0600

    Project Structure

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="h-screen max-w-5xl mx-auto flex flex-col gap-10 items-center justify-center">
+    <h1 className="text-2xl"> WelCome To HireMind </h1>
+    <p>An AI-powered interview preparation platform designed to help job seekers practice, improve, and succeed in technical and non-technical interviews. The application leverages modern AI capabilities to generate interview questions, simulate realistic interview experiences, provide feedback, and track preparation progress.</p>
+    </div>
+  );
+}
